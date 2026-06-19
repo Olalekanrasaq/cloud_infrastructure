cbfdfdcd71d692d11f0226013f40921581c6d51b
Olalekan Rasaq <orasaq377@stu.ui.edu.ng>
2026-06-19 14:37:18 +0100

    Remove the sample demo snowflake database

--- snowflake/main.tf
+++ snowflake/main.tf
@@ -3,11 +3,6 @@ resource "snowflake_database" "cocosurf_sf_db" {
   is_transient = false
 }
 
-resource "snowflake_database" "sample_db" {
-  name         = "SAMPLE_DEMO_DB"
-  is_transient = false
-}
-
 resource "snowflake_warehouse" "cocosurf_sf_wh" {
   name                      = "COCOSURF_WH"
   warehouse_type            = "STANDARD"
